d9d12c214065f19ebc344ff0a9255d467acbf74d
fabioramefiarison <fabioramefiarison@gmail.com>
2024-07-14 15:13:36 +0300

    ajout logique

--- other-page/acceuil.js
+++ other-page/acceuil.js
@@ -1,3 +1,23 @@
+function Get(params) {
+   return document.getElementById(params)
+}
+
+const btnMenu = Get('menu-burger')
+const sideBar = Get("side-bar")
+const section2  = Get("section-2")
+
+let estVisible = true
+btnMenu.addEventListener('click', () =>{
+   if (estVisible) {
+      sideBar.style.display = "block"
+      section2.style.width = "75%"
+   } else {
+      sideBar.style.display = "none"
+      section2.style.width = "100%"
+   }
+   estVisible = !estVisible
+})
+
 function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
